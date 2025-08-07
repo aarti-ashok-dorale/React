1888f73fe4c4582943551a30f4b4299299c7a9c7
aarti-ashok-dorale <aartidorale2004@gmail.com>
2025-08-07 21:11:25 +0530

    Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 import Counter from "./Counter";
 import LikeButton from "./LikeButton";
 import LudoBoard from './LudoBoard';
+import TodoList from './TodoList';
+
 
 function App() {
  return ( 
   <>
-    <LudoBoard />
+    <TodoList />
   </>
  );
 }
---
+++ src/TodoList.jsx
@@ -0,0 +1,37 @@
+import {useState} from "react";
+
+export default function TodoList() {
+    let [todos, setTodos] = useState([]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTask = () => {
+        setTodos([...todos, newTodo]);
+    };
+
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value);
+    };
+
+        return(
+        <div>
+            <input placeholder="add a task" 
+            value={newTodo}
+            onChange={updateTodoValue}></input>
+            <br></br>
+            <br></br>
+            <button onClick={addNewTask} >Add Task</button>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <h4>Task Todo</h4>
+            <ul>
+                {
+                    todos.map((todo) => (
+                        <li>{todo}</li>
+                    )
+                )}
+            </ul>
+        </div>
+    );
+}
