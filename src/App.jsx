570dcfc2120019c611c478e6331d6bfaf936ab8c
aarti-ashok-dorale <aartidorale2004@gmail.com>
2025-08-16 19:58:38 +0530

    useEffect Hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './App.css';
 import Form from "./Form";
 import CommentsForm from './CommentsForm.jsx';
 import Comment from './Comment.jsx';
-
+import Counter from './Counter.jsx';
+import Joker from './Joker.jsx';
 function App() {
  
 
   return (
     <>
-      <Comment />
+      <Joker />
     </>
   );
 }
